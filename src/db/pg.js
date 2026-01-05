4ef849a863784aa6324f42505a9bdb54e9e8bd42
JoseAv <jm.aranav@gmail.com>
2026-01-05 16:40:05 -0600

    Connection db and config

--- src/db/pg.js
+++ src/db/pg.js
@@ -1,4 +1,14 @@
-const sequelize = new Sequelize('postgre', 'admin', 'admin', {
+import { Sequelize } from "sequelize";
+
+export const db = new Sequelize('postgres', 'admin', 'admin', {
     host: 'localhost',
     dialect: 'postgres'
 })
+
+
+try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
